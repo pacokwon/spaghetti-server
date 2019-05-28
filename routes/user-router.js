98c85fd6fb08d7d4bbed555039bc1bff56a43bca
pacokwon <haechank@gmail.com>
2019-05-28 22:16:30 +0900

    fixed minor router typo

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 
